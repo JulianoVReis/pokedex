ec128278c1b875ace18743eedff70acbf8de6bf4
Juliano Reis <julianovreis@gmail.com>
2025-06-18 12:16:04 -0300

    update pokedex

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let pokemonNome = document.querySelector('.nome')
-let pokemonNumero = document.querySelector('.numero')
-let pokemonImg = document.querySelector('.pokemon')
-let form = document.querySelector('.form')
-let input = document.querySelector('.pesquisar')
-let proxBotao = document.querySelector('.btn-ant')
-let depBotao = document.querySelector('.btn-dep')
+let pokemonNome = document.querySelector(".nome")
+let pokemonNumero = document.querySelector(".numero")
+let pokemonImg = document.querySelector(".pokemon")
+let form = document.querySelector(".form")
+let input = document.querySelector(".pesquisar")
+let proxBotao = document.querySelector(".btn-ant")
+let depBotao = document.querySelector(".btn-dep")
 let procurarPokemon = 1
 
 let fetchPokemon = async (pokemon) => {
@@ -12,43 +12,43 @@ let fetchPokemon = async (pokemon) => {
 
 	if (APIResponse.status == 200) {
 		let data = await APIResponse.json()
-		return data
+		return data;
 	}
 }
 
 let renderPokemon = async (pokemon) => {
-	pokemonNome.innerHTML = 'Loading...'
-	pokemonNumero.innerHTML = ''
+	pokemonNome.innerHTML = "Loading..."
+	pokemonNumero.innerHTML = ""
 	let data = await fetchPokemon(pokemon)
 
 	if (data) {
-		pokemonImg.style.display = 'block'
+		pokemonImg.style.display = "block"
 		pokemonNome.innerHTML = data.name
 		pokemonNumero.innerHTML = data.id
-		pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-		input.value = ''
+		pokemonImg.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
+		input.value = ""
 		procurarPokemon = data.id
 	} else {
-		pokemonImg.style.display = 'none'
-		pokemonNome.innerHTML = 'Not Found... :('
-		pokemonNumero.innerHTML = ''
+		pokemonImg.style.display = "none"
+		pokemonNome.innerHTML = "Not Found... :("
+		pokemonNumero.innerHTML = ""
 	}
 }
 
-form.addEventListener('submit', (event) => {
-	event.preventDefault()
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
 	renderPokemon(input.value.toLowerCase())
 })
 
-proxBotao.addEventListener('click', (event) => {
+proxBotao.addEventListener("click", (event) => {
 	if (procurarPokemon > 1) {
 		procurarPokemon -= 1
 		renderPokemon(procurarPokemon)
 	}
 })
 
-depBotao.addEventListener('click', (event) => {
-	procurarPokemon += 1
+depBotao.addEventListener("click", (event) => {
+	procurarPokemon += 1;
 	renderPokemon(procurarPokemon)
 })
 
